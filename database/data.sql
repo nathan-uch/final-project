4d041ff1b5939a7ad5de6670cc4e12be9c6de84c
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-07-29 07:25:35 +0000

    Added exercise data as placeholder. #2

--- database/data.sql
+++ database/data.sql
@@ -2,4 +2,11 @@ insert into "users" ("username", "hashedPassword", "createdAt")
 values ('admin', 'a', '2022-07-27T16:01:32.134Z');
 
 insert into "exercise" ("exerciseId", "name", "muscleGroup", "equipment")
-values (1, 'bench press', 'chest', 'barbell');
+values (1, 'bench press', 'chest', 'barbell'),
+       (2, 'pull ups', 'back', 'null'),
+       (3, 'deadlift', 'hamstrings', 'barbell'),
+       (4, 'squats', 'quads', 'barbell'),
+       (5, 'incline press', 'chest', 'barbell'),
+       (6, 'lat pulldown', 'back', 'cable bar'),
+       (7, 'overhead press', 'deltoids', 'dumbbell'),
+       (8, 'leg press', 'quads', 'machine');
