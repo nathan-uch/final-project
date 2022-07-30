22d06ac85fd869827efd2fd76379dde58653fde2
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-07-30 00:02:09 +0000

    Added fetch request and rendering of exercises in component.

--- client/pages/exercises.jsx
+++ client/pages/exercises.jsx
@@ -1,33 +1,37 @@
 import React, { useState, useEffect } from 'react';
 
-function ExerciseCard(props) {
+function ExerciseCard({ name }) {
 
   return (
-    <div>
-      <p className="has-background-light">{}</p>
+    <div className="box has-background-grey-lighter">
+        <p className="title">{name}</p>
     </div>
   );
 }
 
 export default function Exercises(props) {
   const [exercises, setExercises] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('/api/all-exercises')
       .then(response => response.json())
       .then(data => {
         setExercises(data);
+        setLoading(false);
       })
       .catch(err => console.error('ERROR:', err));
-  });
+  }, []);
 
   return (
     <div className="body-container">
       <h3>Add Exercise</h3>
         <div>
-          {exercises.map(exercise =>
-            <ExerciseCard key={exercise.name} />
-          )}
+          {isLoading
+            ? <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+            : exercises.map(exercise =>
+              <ExerciseCard key={exercise.name} name={exercise.name} />
+            )}
         </div>
     </div>
   );
