1f49c94bd71e666de5846028bdcfa305c87eee94
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-07-31 03:11:03 +0000

    Toggle is displaying from selecting exercise from list. #3

--- client/pages/exercises.jsx
+++ client/pages/exercises.jsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
-function ExerciseCard({ name }) {
+function ExerciseCard({ name, allSelected, setAllSelected }) {
+  const [isSelected, setSelected] = useState(false);
+
+  function handleClick() {
+    !isSelected ? setSelected(true) : setSelected(false);
+    setAllSelected({ ...allSelected, name });
+  }
 
   return (
-    <a className="column is-size-5-mobile mx-5 is-two-fifths is-flex-direction-row is-flex-wrap-wrap exercise-card box has-background-grey-lighter has-text-centered">
-        <p className="title">{name}</p>
+    !isSelected
+      ? <a onClick={handleClick} className="exercise-card has-background-grey-lighter column is-size-5-mobile mx-5 is-two-fifths is-flex-direction-row is-flex-wrap-wrap exercise-card box has-text-centered">
+        <p className="title is-size-4">{name}</p>
+    </a>
+      : <a onClick={handleClick} className="selected-exercise-card has-background-white column is-size-5-mobile mx-5 is-two-fifths is-flex-direction-row is-flex-wrap-wrap exercise-card box  has-text-centered">
+        <p className="title is-inline is-size-4">{name}</p>
+      <i className='fa-solid fa-check fa-2x mr-4'></i>
     </a>
   );
 }
@@ -12,6 +23,7 @@ function ExerciseCard({ name }) {
 export default function Exercises(props) {
   const [exercises, setExercises] = useState(null);
   const [isLoading, setLoading] = useState(true);
+  const [allSelected, setAllSelected] = useState({});
 
   useEffect(() => {
     fetch('/api/all-exercises')
@@ -31,7 +43,7 @@ export default function Exercises(props) {
           {isLoading
             ? <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
             : exercises.map(exercise =>
-              <ExerciseCard key={exercise.name} name={exercise.name} />
+              <ExerciseCard key={exercise.name} name={exercise.name} setAllSelected={setAllSelected} allSelected={allSelected} />
             )}
         </div>
     </div>
