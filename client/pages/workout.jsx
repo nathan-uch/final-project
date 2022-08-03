c45690c2c61a440d7479fee74901245599e2046c
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-08-03 04:30:53 +0000

    Created new components for exercises and sets. #5

--- client/pages/workout.jsx
+++ client/pages/workout.jsx
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
-function Exercise() {
+function Set() {
+
+  return (
+    <form className="mb-2 has-text-centered is-flex is-justify-content-space-between is-align-content-flex-start">
+      <p className="is-size-4 set-num">1</p>
+      <input type="number" min="1" className="mx-2 reps-input has-text-centered is-size-4 py-2 has-background-grey-lighter" />
+      <input type="number" min="1" className="mx-2 weight-input has-text-centered is-size-4 py-2 has-background-grey-lighter" />
+      <button href="#" className="set-done-btn has-background-white" ><i className="fa-solid fa-check fa-3x mx-4 unselected-check"></i></button>
+    </form>
+  );
+}
+
+function Exercise({ name }) {
 
   return (
     <div className="card mb-5">
       <div className="card-header">
-        <h3 className="exercise-name card-header-title has-background-black has-text-weight-semibold is-size-4 is-justify-content-center">Bench Press</h3>
+        <h3 className="exercise-name card-header-title has-background-black has-text-weight-semibold is-size-4 is-justify-content-center">{name}</h3>
       </div>
       <div className="card-content pt-3 pb-0">
         <div className="mb-4 has-text-centered is-flex is-justify-content-space-between is-align-content-flex-start">
@@ -14,21 +26,7 @@ function Exercise() {
           <p className="mx-3 weight-title is-inline is-size-5 has-text-weight-semibold">Weight</p>
           <p className="mx-3 exer-done-title is-inline is-size-5 has-text-weight-semibold">Done</p>
         </div>
-
-        {/* SET 1 */}
-        <form className="mb-2 has-text-centered is-flex is-justify-content-space-between is-align-content-flex-start">
-          <p className="is-size-4 set-num">1</p>
-          <input type="number" min="1" className="mx-2 reps-input has-text-centered is-size-4 py-2 has-background-grey-lighter" />
-          <input type="number" min="1" className="mx-2 weight-input has-text-centered is-size-4 py-2 has-background-grey-lighter" />
-          <button href="#" className="set-done-btn has-background-white" ><i className="fa-solid fa-check fa-3x mx-4 unselected-check"></i></button>
-        </form>
-        {/* SET 2 */}
-        <form className="mb-2 has-text-centered is-flex is-justify-content-space-between is-align-content-flex-start">
-          <p className="is-size-4 set-num">2</p>
-          <input type="number" min="1" className="mx-2 reps-input has-text-centered is-size-4 py-2 has-background-grey-lighter" />
-          <input type="number" min="1" className="mx-2 weight-input has-text-centered is-size-4 py-2 has-background-grey-lighter" />
-          <button href="#" className="set-done-btn has-background-white"><i className="fa-solid fa-check fa-3x mx-4 unselected-check"></i></button>
-        </form>
+        <Set />
       </div>
       <div className="card-footer">
         <button type="button" className="add-set-btn button is-size-5 my-2 has-background-grey-lighter">Add set</button>
@@ -38,8 +36,7 @@ function Exercise() {
 }
 
 export default function WorkoutPage(props) {
-  const [exercises, setExercises] = useState();
-
+  const [exercises, setExercises] = useState(null);
   const workoutId = 1;
 
   useEffect(() => {
@@ -55,9 +52,11 @@ export default function WorkoutPage(props) {
     <div className='body-container has-text-centered'>
       <h3 className="is-inline is-size-3 has-text-weight-semibold">New Workout</h3>
       <div className='mt-5 is-flex is-align-items-center is-flex-direction-column'>
-        {exercises.map(exercise =>
-          <Exercise key={exercise.exerciseId} />
-        )}
+        {!exercises
+          ? <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+          : exercises.map((exercise, index) =>
+          <Exercise key={index} name={exercise.name} />
+          )}
       </div>
     </div>
   );
