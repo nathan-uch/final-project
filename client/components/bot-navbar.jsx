d1eece803c0587a7a91c0b6e926242c1f1f94771
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-08-12 06:12:44 +0000

    Removed styling that made navbar break in safari.

--- client/components/bot-navbar.jsx
+++ client/components/bot-navbar.jsx
@@ -6,12 +6,12 @@ export default function BotNavbar() {
     <nav className="navbar is-hidden-desktop px-5 is-fixed-bottom has-background-black" role="navigation" aria-label="main navigation">
       <div className="navbar-menu is-active is-flex is-align-items-center is-flex-direction-row is-flex-wrap-nowrap is-justify-content-space-around p-0">
         <a href="#user-profile" className="navbar-item p-0">
-          <figure className="image navbar-icon">
+          <figure className="navbar-icon">
             <img src="images/user-icon.png" className="bot-navbar-icon my-2 nav-user-icon" />
           </figure>
         </a>
         <a href="#new-workout" className="navbar-item p-0">
-          <figure className="image navbar-icon">
+          <figure className="navbar-icon">
             <img src="images/db-yellow.png" className="bot-navbar-icon my-2" />
           </figure>
         </a>
