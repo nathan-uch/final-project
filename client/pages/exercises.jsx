bf07ce0dc72be18890b226901a3ca9d72675f880
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-08-24 18:09:14 +0000

    Added searchbox to exercise page. #12

--- client/pages/exercises.jsx
+++ client/pages/exercises.jsx
@@ -162,6 +162,7 @@ export default function Exercises(props) {
     <>
       <div className="body-container has-text-centered">
         <h3 className="is-inline-block is-size-3-mobile is-size-2 mx-auto mb-5">Add Exercises</h3>
+        <input type="search" className="exercise-searchbox is-block mx-auto mb-6 py-2 px-4 is-size-5" placeholder="Search exercises" />
         <div className="alphabet-container mb-5 columns is-flex is-flex-wrap-wrap is-justify-content-center">
           {letters && letters.map(letter =>
             <AlphabetButtons key={letter} letter={letter} />
